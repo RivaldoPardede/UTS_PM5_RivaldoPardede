d00b8b776b5be57cd9b665a34ad38bafb492051e
RivaldoPardede <usurivaldo@gmail.com>
2024-10-28 00:06:56 +0700

    welcomingPage logic complete

--- app/src/main/java/com/example/netwise/MainActivity.kt
+++ app/src/main/java/com/example/netwise/MainActivity.kt
@@ -1,19 +1,43 @@
 package com.example.netwise
 
 import BaseActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import android.widget.ToggleButton
 
 class MainActivity : BaseActivity() {
 
+    private lateinit var editUsername: EditText
+    private lateinit var startBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_result)
+        setContentView(R.layout.activity_main)
 
-        // Get the theme toggle button
+        editUsername = findViewById(R.id.editUsername)
+        startBtn = findViewById(R.id.startBtn)
+
+        startBtn.setOnClickListener {
+            validateAndProceed()
+        }
+
+        // Theme Toggle Logic
         val themeToggle = findViewById<ToggleButton>(R.id.themeToggle)
-
-        // Call the setupThemeToggle with the toggle button reference
         setupThemeToggle(themeToggle)
+    }
+
+    private fun validateAndProceed() {
+        val username = editUsername.text.toString().trim()
+
+        if (username.isEmpty()) {
+            Toast.makeText(this, "Please enter your username first.", Toast.LENGTH_SHORT).show()
+        } else {
+            val intent = Intent(this, Quiz::class.java)
+            intent.putExtra("PLAYER_NAME", username)
+            startActivity(intent)
+        }
     }
 }
